47361d3b190663d14f8dbb7ac94160bb0e852035
rikvik2006 <75540124+rikvik2006@users.noreply.github.com>
2022-05-31 15:59:33 +0200

    created the home

---
+++ src/components/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./home.css";
+
+export default function Home() {
+  return (
+    <div className="container-sm">
+      <div className="box">
+          <p className="weArriveSoon">We are almost<br />there</p>
+      </div>
+    </div>
+  )
+}
